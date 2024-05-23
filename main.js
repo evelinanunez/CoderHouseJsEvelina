656eb5cec62a3726e733a1de549e364ffd038b86
Evelina <evenunez@alumno.unlam.edu.ar>
2024-05-22 22:19:25 -0300

    se modifica estilos y se comienza a controlar el html mediante el dom

--- main.js
+++ main.js
@@ -5,31 +5,31 @@ varios de jardineria.
 
 const productos = [
     {
-        categoria : 'Cactus',
-        nombre: "Opuntia",
-        descripcion : "También conocidos como cactus de la chumbera o cactus de paddle. Tienen almohadillas planas y redondeadas que se asemejan a paletas. Algunas especies tienen espinas mientras que otras tienen gloquidios (pequeñas espinas diminutas).",
-        precio : 1000
+        categoriaProducto : "cactus",
+        nombreProducto: "Opuntia",
+        imagenProducto: "./img/cactus.png",
+        precioProducto : 1000
     },
     {
-        categoria : "cactus",
-        nombre: "Echinocactus",
-        descripcion : 'Conocidos como cactus de barril, son redondos o cilíndricos con espinas grandes y prominentes. Algunas especies, como el Echinocactus grusonii, tienen una apariencia distintiva de "bola de oro".',
-        precio : 800
+        categoriaProducto : "cactus",
+        nombreProducto: "Echinocactus",
+        imagenProducto: "./img/cactus.png",
+        precioProducto : 800
     },
     {
-        categoria : "cactus",
-        nombre: "Epiphyllum",
-        descripcion : 'A diferencia de la mayoría de los cactus, estas especies son epífitas y crecen en árboles en lugar de en el suelo. Tienen hojas planas y producen flores grandes y llamativas.',
-        precio : 300
+        categoriaProducto: "cactus",
+        nombreProducto: "Epiphyllum",
+        imagenProducto: "./img/cactus.png",
+        precioProducto : 300
     }
 
 ];
 
-function convertirAMinuscula(texto){
+/* function convertirAMinuscula(texto){
     return texto.toLocaleLowerCase();
-}
+} */
 
-function agregarProducto(categoria, nombre, descripcion, precio){
+/* function agregarProducto(categoria, nombre, descripcion, precio){
     const producto = {
         categoria : categoria,
         nombre: nombre,
@@ -37,9 +37,9 @@ function agregarProducto(categoria, nombre, descripcion, precio){
         precio : precio
     }
     productos.push(producto);
-}
+} */
 
-function editarProducto(propiedadParaEditar, nombreProductoAEditar){
+/* function editarProducto(propiedadParaEditar, nombreProductoAEditar){
   for(i = 0; i < productos.length ; i++){
     if(convertirAMinuscula(productos[i].nombre) == convertirAMinuscula(nombreProductoAEditar)){
         let continuarEditando = confirm("Quiere continuar con la edición de datos de un producto? ");
@@ -93,38 +93,63 @@ function editarProducto(propiedadParaEditar, nombreProductoAEditar){
     }
 
   }
-}
+} */
 
-
-function eliminarProducto(nombreProductoAEliminar){
+/* function eliminarProducto(nombreProductoAEliminar){
     for( i = 0 ; i < productos.length ; i++){
         if(convertirAMinuscula(productos[i].nombre) == convertirAMinuscula(nombreProductoAEliminar)){
             const nombreProductorEliminado = productos[i].nombre;
             productos.splice(i,1);
+            console.log(i);
             console.log("Se elimino el producto " + nombreProductorEliminado);
             break;
         }
     }
-}
+} */
 
-function verProducto(nombreProductoAMostrar){
+/* function verProducto(nombreProductoAMostrar){
     for( i = 0 ; i < productos.length ; i++){
         if(convertirAMinuscula(productos[i].nombre) == convertirAMinuscula(nombreProductoAMostrar)){
             console.log("Los datos del producto " + nombreProductoAMostrar+ " son los siguientes:  Nombre producto: " + productos[i].nombre + ", Categoria producto: " + productos[i].categoria +
         ", Descripción producto: " + productos[i].descripcion + ", Precio: " + productos[i].precio )
         }
     }
-}
+} */
 
-function verProductos(){
+/* function verProductos(){
     for (const producto of productos) {
         console.log("Los datos del producto son los siguientes:  Nombre producto: " + producto.nombre + ", Categoria producto: " + producto.categoria +
         ", Descripción producto: " + producto.descripcion+ ", Precio: " + producto.precio );
     }
-}
+} */
 
-function main(){
-    let continuar = confirm("Presione Aceptar para Iniciar/continuar o Cancelar para Finalizar la aplicación.");
+const crearCard = (cactus)=>{
+    const app = document.getElementById("app");
+    const element = document.createElement("div");
+    element.className = "estilosContenedor"
+    element.innerHTML = `
+                        <div class="card" style="width: 18rem;">
+                            <img src="${cactus.imagenProducto}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h2 class="card-title">${cactus.nombreProducto}</h2>
+                                <h3>Categoria:  ${cactus.categoriaProducto}</h3>
+                                <button type="button" class="btn btn-dark">Editar</button>
+                                <button type="button" class="btn btn-dark" id="btnAgregarProducto">Eliminar</button>
+                            </div>
+                        </div>
+                        `
+    app.append(element)
+}
+const main = () =>{
+    productos.forEach((cactus)=>{
+        crearCard(cactus);
+    })
+    const formularioAgregar = document.getElementById("formularioAgregar");
+    formularioAgregar.className ="formularioAgregarProducto";
+    const btnAgregarProducto = document.addEventListener("btnAgregarProducto");
+    btnAgregarProducto.addEventListener("click" ,()=>{
+    })
+ /*    let continuar = confirm("Presione Aceptar para Iniciar/continuar o Cancelar para Finalizar la aplicación.");
     while(continuar){
     const accionARealizar = prompt("¡Bienvenido! Seleccione alguna de las siguientes acciones: agregar, editar, eliminar, producto, todosProductos ");
         if(convertirAMinuscula(accionARealizar) == "agregar"){
@@ -150,7 +175,7 @@ function main(){
             alert("Accion no valida")
         }
        continuar = confirm("Presione Aceptar para Iniciar/continuar o Cancelar para Finalizar la aplicación.");
-    }
+    } */
 }
 
 main();
